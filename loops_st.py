f892e60c32a693d7e967c381a79f5a0dd0de8a6a
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2023-12-11 17:33:40 +0530

    Add files via upload

---
+++ loops_st.py
@@ -0,0 +1,33 @@
+# for loop
+# change the name in list
+list = [1,2,"besangth",4]
+for i in range(4):
+    if list[i] == "besangth":
+        list[i]="vani"
+    print(list[i])
+print(list)
+
+# list view
+list = [1,2,"besangth",4]
+for s in list:
+    print(s)
+
+# loop in break
+list = [1,2,"besangth",4]
+for s in list:
+    print(s)
+    if s == "besangth":
+        break
+
+# loops in print 10 ti
+for i in range(10):
+    print("hello world")
+
+# for num in loops
+for num in range(21):
+    print(num)
+
+
+
+
+
